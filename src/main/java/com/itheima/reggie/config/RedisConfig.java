33937dfb64d59e96e30a00e4a66d025fe2005949
week_z0127 <23166321.com>
2022-04-12 16:19:41 +0800

    Redis配置

---
+++ src/main/java/com/itheima/reggie/config/RedisConfig.java
@@ -0,0 +1,27 @@
+package com.itheima.reggie.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @author 张军
+ * @version 1.0.0
+ * @ClassName RedisConfig.java
+ * @Description
+ * @createTime 2022年04月12日 16:18:00
+ */
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        //默认的Key序列化器为：JdkSerializationRedisSerializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
